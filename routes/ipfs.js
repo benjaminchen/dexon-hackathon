846d38f13ac8bb77e70e7938c196f51af748a2c5
benjamin <benjaminchen.c@gmail.com>
2018-12-16 12:48:16 +0800

    ipfs update

--- routes/ipfs.js
+++ routes/ipfs.js
@@ -3,28 +3,34 @@ const ipfsAPI = require('ipfs-http-client')
 const fs = require('fs')
 const DexonDecimal = 10e17
 const ipfs = ipfsAPI('bookdemo.hopto.org', '5001', {protocol: 'http'})
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/'
+})
 
 module.exports = function(web3) {
+    let crypto = require('crypto'),
+        algorithm = 'aes-256-ctr',
+        password = 'd6F3Efeq'
 
-    routes.get('/file', (req, res) => {
-        ipfs.addFromURL('https://www.google.com/', function (err, file) {
-            if (err) {
-              throw err
-            }
-
-            console.log(file)
-            res.json(file)
-        })
+    routes.post('/book/file', upload.single('file-to-upload'), async (req, res) => {
+        var contents = fs.readFileSync(req.file.path)
+        var cipher = crypto.createCipher(algorithm, password)
+        var crypted = Buffer.concat([cipher.update(contents),cipher.final()]);
+        let results = await ipfs.add(crypted)
+        console.log(results)
+        res.json(results)
     })
 
-    routes.get('/files', (req, res) => {
-        ipfs.get('Qmck3HJQ6BDWhBWUJbkzKJmGAroJdaPJjWZfQ7XCpVBmVc', function (err, files) {
+    routes.get('/files/:hash', (req, res) => {
+        let hash = req.params.hash
+        var decipher = crypto.createDecipher(algorithm, password)
+        ipfs.get(hash, function (err, files) {
             files.forEach((file) => {
-              console.log(file.path)
-              console.log(file.content.toString('utf8'))
-              res.send(file.content.toString('utf8'))
+                var dec = Buffer.concat([decipher.update(file.content) , decipher.final()])
+                res.send(dec)
             })
-          })
+        })
     })
 
     return routes
